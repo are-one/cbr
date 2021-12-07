d683747868af9c60a45bbd1498337615e2eb1e4b
Are1PM <arwanpriantomangidi@gmail.com>
2021-12-07 23:01:24 +0800

    [UPDATE] change title

--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Sistem Pakar Metode BC - CBR';
 ?>
 <div class="page-header">
     <h1 class="entry-title">Sistem Pakar Penyakit Gizi Buruk pada Balita Menggunakan Metode Case Based Reasoning dan
@@ -12,6 +12,6 @@ $this->title = 'My Yii Application';
     <p>Metode case based reasoning adalah salah satu metode untuk membangun sistem dengan pengambilan keputusan dari
         kasus yang baru dengan berdasarkan solusi dari kasus – kasus sebelumnya. Konsep dari metode case based
         reasoning ditemukan dari ide untuk menggunakan pengalaman – pengalaman yang terdokumentasi untuk
-        menyelesaikan masalah yang baru. Para decisionmaker kebanyakan menggunakan pengalaman – pengalaman dari
+        menyelesaikan masalah yang baru. Para decision maker kebanyakan menggunakan pengalaman – pengalaman dari
         problem solving terdahulu untuk menyelesaikan masalah yang dihadapi sekarang.</p>
 </div>
