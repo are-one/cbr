e3d632215c6f3b06da7c920df7079fdb8ea5e9b8
Are1PM <arwanpriantomangidi@gmail.com>
2021-12-07 16:42:45 +0800

    [UPDATE] Menambahkan RBAC

--- config/web.php
+++ config/web.php
@@ -11,6 +11,11 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ]
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -51,6 +56,9 @@ $config = [
                 ],
             ],
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager'
+        ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
@@ -59,6 +67,19 @@ $config = [
             ],
         ],
         */
+        'as access' => [
+            'class' => 'mdm\admin\components\AccessControl',
+            'allowActions' => [
+                'site/*',
+                'admin/*',
+                'some-controller/some-action',
+                // The actions listed here will be allowed to everyone including guests.
+                // So, 'admin/*' should not appear here in the production, of course.
+                // But in the earlier stages of your development, you may probably want to
+                // add a lot of actions here until you finally completed setting up rbac,
+                // otherwise you may not even take a first step.
+            ]
+        ],
     ],
     'params' => $params,
 ];
